a5c75f97d3930ff112318cd10f95a601396f0ced
Arthur Denner <arthurdenner7@gmail.com>
2020-08-05 22:12:24 +0200

    feat(home): create header widget

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'home_controller.dart';
+import 'widgets/header.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
@@ -13,11 +14,30 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomePage'),
-      ),
-      body: Center(
-        child: Text('Solar System'),
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            alignment: Alignment.centerRight,
+            fit: BoxFit.cover,
+            image: AssetImage(
+              'assets/backgrounds/background-constellation.png',
+            ),
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 32,
+            ),
+            child: Column(
+              children: <Widget>[
+                HomeHeader(username: 'Arthur Denner'),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
