44925008e7a2aad0749406b4779843364942cbbb
Arthur Denner <arthurdenner7@gmail.com>
2020-08-06 22:26:43 +0200

    fix(details): add correct scroll behavior to details page

--- lib/app/pages/details/details_page.dart
+++ lib/app/pages/details/details_page.dart
@@ -22,53 +22,85 @@ class DetailsPage extends StatelessWidget {
     final _textTheme = Theme.of(context).textTheme;
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: DetailsPageWrapper(
-          details: details,
-          child: Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 32,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SearchHeader(),
-                SizedBox(height: _height * .35),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      details.name,
-                      style: _textTheme.headline4.copyWith(
-                        color: AppColors.brand,
+      body: DetailsPageWrapper(
+        details: details,
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 32,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SearchHeader(),
+              SizedBox(height: _height * .32),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            details.name,
+                            style: _textTheme.headline4.copyWith(
+                              color: AppColors.brand,
+                            ),
+                          ),
+                          Row(
+                            children: <Widget>[
+                              BookmarkIcon(
+                                color: AppColors.brand,
+                                saved: details.saved,
+                              ),
+                              SizedBox(width: 16),
+                              SvgPicture.asset(
+                                'assets/icons/share.svg',
+                                color: AppColors.brand,
+                                width: 24,
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                    ),
-                    Row(
-                      children: <Widget>[
-                        BookmarkIcon(
-                          color: AppColors.brand,
-                          saved: details.saved,
+                      SizedBox(height: 40),
+                      Text(
+                        details.description,
+                        style: _textTheme.bodyText1.copyWith(
+                          color: AppColors.brand.withOpacity(.75),
                         ),
-                        SizedBox(width: 16),
-                        SvgPicture.asset(
-                          'assets/icons/share.svg',
-                          color: AppColors.brand,
-                          width: 24,
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-                SizedBox(height: 40),
-                Text(
-                  details.description,
-                  style: _textTheme.bodyText1.copyWith(
-                    color: AppColors.brand.withOpacity(.75),
+                      ),
+                      SizedBox(height: 40),
+                      ExpansionTile(
+                        trailing: Container(width: 10),
+                        title: Row(
+                          children: <Widget>[
+                            SvgPicture.asset(
+                              'assets/icons/down.svg',
+                              color: AppColors.brand,
+                              width: 16,
+                            ),
+                            SizedBox(width: 16),
+                            Text(
+                              'Introdução',
+                              style: _textTheme.button.copyWith(
+                                color: AppColors.brand,
+                              ),
+                            ),
+                          ],
+                        ),
+                        children: <Widget>[
+                          Text(
+                            'Lorem ipsum',
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
