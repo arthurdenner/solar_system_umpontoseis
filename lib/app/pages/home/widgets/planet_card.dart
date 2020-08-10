ca822033de1abd0416439284ba33c3776726de71
Arthur Denner <arthurdenner7@gmail.com>
2020-08-10 18:50:13 +0200

    feat(home): go to details page when planet is clicked

--- lib/app/pages/home/widgets/planet_card.dart
+++ lib/app/pages/home/widgets/planet_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:solar_system_umpontoseis/app/shared/models/planet_details.dart';
 import 'package:solar_system_umpontoseis/app/shared/utils/colors.dart';
@@ -17,51 +18,62 @@ class PlanetCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(8),
-      child: Stack(
-        children: <Widget>[
-          Container(
-            height: 190,
-            width: 140,
-            padding: EdgeInsets.all(12),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              color: AppColors.brand,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      details.name,
-                      style: Theme.of(context).textTheme.button,
-                    ),
-                    GradientMask(
-                      gradient: AppColors.gradientButton,
-                      child: SvgPicture.asset(
-                        'assets/icons/forward.svg',
-                        width: 16,
+    return GestureDetector(
+      onTap: _selectPlanet,
+      behavior: HitTestBehavior.opaque,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: 190,
+              width: 140,
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: AppColors.brand,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        details.name,
+                        style: Theme.of(context).textTheme.button,
                       ),
-                    ),
-                  ],
+                      GradientMask(
+                        gradient: AppColors.gradientButton,
+                        child: SvgPicture.asset(
+                          'assets/icons/forward.svg',
+                          width: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              left: left - 50,
+              top: -30,
+              child: Hero(
+                tag: details.name,
+                child: Image.asset(
+                  details.image,
+                  fit: BoxFit.fill,
+                  height: 150,
                 ),
-              ],
+              ),
             ),
-          ),
-          Positioned(
-            left: left - 50,
-            top: -30,
-            child: Image.asset(
-              details.image,
-              fit: BoxFit.fill,
-              height: 150,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
+  }
+
+  void _selectPlanet() {
+    Modular.to.pushNamed('/details', arguments: {"details": details});
   }
 }
